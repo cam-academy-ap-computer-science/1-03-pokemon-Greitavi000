59b3afc0dbccaabeaa7ef55323b3a30469492dbe
greitavi000 <greitavi000@CAM-LAB-16.bgsd.k12.wa.us>
2019-09-09 07:26:09 -0700

    Pokemon.java

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,7 +3,10 @@ public class Pokemon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(" Pikachu welkomes you to the world of Pokemon!");
+		System.out.println(" (\\__/)");
+		System.out.println(" (o^.^)");
+		System.out.println("z(_(\")(\")");
 	}
 
 }
